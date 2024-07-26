74f2c2be3b048ab37f208aa6f8b931f47ba9f6e3
tiutcristian <107301343+tiutcristian@users.noreply.github.com>
2024-07-26 20:49:12 +0300

    initial commit

---
+++ mobile-clone/src/main/java/ro/msg/mobile_clone/MobileCloneApplication.java
@@ -0,0 +1,13 @@
+package ro.msg.mobile_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileCloneApplication.class, args);
+	}
+
+}
