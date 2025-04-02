c1c3958cd60d53c0dfc32ca8b466bca79e487f95
Cristian Tiut <tiutcristian@gmail.com>
2025-04-03 01:37:36 +0300

    Create HeartbeatController.java

---
+++ src/main/java/ro/msg/mobile_clone/rest/controller/HeartbeatController.java
@@ -0,0 +1,20 @@
+package ro.msg.mobile_clone.rest.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/v1/heartbeat")
+@AllArgsConstructor
+public class HeartbeatController {
+    @GetMapping
+    public ResponseEntity<Void> check() {
+        return ResponseEntity.ok().build();
+    }
+}
