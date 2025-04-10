5d3584e289bb87bf77d2c771227c17e6daa548a9
Cristian Tiut <tiutcristian@gmail.com>
2025-04-10 16:38:49 +0300

    Create HeartbeatController + Add LAN IP to CORS origins

---
+++ src/main/java/ro/msg/mobile_clone/rest/controller/HeartbeatController.java
@@ -0,0 +1,19 @@
+package ro.msg.mobile_clone.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = {"http://192.168.254.225:3000", "http://localhost:3000"})
+@RequestMapping("/api/v1/heartbeat")
+@AllArgsConstructor
+public class HeartbeatController {
+    @GetMapping
+    public ResponseEntity<Void> check() {
+        return ResponseEntity.ok().build();
+    }
+}
